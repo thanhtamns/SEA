f95a2f40ec20d5326590e912a20741390f0ce905
Nguyễn Văn Kính <nguyenvankinh181196@gmail.com>
2016-01-01 11:19:13 +0700

    Post Admin
    hoàn thành phần post của trong admin

--- SEA/SEA/Areas/Admin/AdminAreaRegistration.cs
+++ SEA/SEA/Areas/Admin/AdminAreaRegistration.cs
@@ -12,13 +12,14 @@ namespace SEA.Areas.Admin
             }
         }
 
-        public override void RegisterArea(AreaRegistrationContext context) 
+        public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
                 new {controller = "AccountAdmin", action = "Login", id = UrlParameter.Optional }
             );
+
         }
     }
 }
